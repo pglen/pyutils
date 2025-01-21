d3554eeb06e1fdf7c5a70a36bb1eddea6d407b6a
Peter Glen <peterglen@gmail.com>
2025-01-21 03:02:52 -0500

    misc_testdive

--- testdrive.py
+++ testdrive.py
@@ -3,49 +3,26 @@
 import  os, sys, getopt, signal, select, socket, time, struct
 import  random, stat, os.path, datetime, threading, subprocess
 import  struct, io, traceback, hashlib, traceback, argparse
-
-try:
-    import fcntl
-except:
-    fcntl = None
+import codecs
 
 base = os.path.dirname(os.path.realpath(__file__))
 
-#print("testdrive")
+'''
 
-# String triplet per test.
-#
-# Format:
-#
-#   Context string  Send string  Expect string
-#   --------------  -----------  -------------
+ String quaruplet per test.  Input format is array of data.
 
-test_case = [ \
-    ["", "./dbaseadm.py -k test -a testdata", b""],
+    #   Context_string  Send_string     Expect_string   Find/Compare
+    #   --------------  -----------     -------------   ------------
+    #    for the user   what to test    what to expect  True if Find
 
-    [ "Dump data", "./dbaseadm.py -m",
-           b"0     pos    32 Data: b'test' Data2: b'testdata'\n"],
+'''
 
-    [ "Create data2", "./dbaseadm.py -k test2 -a testdata2", b""],
-
-    ["Dump data", "./dbaseadm.py -m",
-         b"0     pos    68 Data: b'test2' Data2: b'testdata2'\n"\
-         b"1     pos    32 Data: b'test' Data2: b'testdata'\n"],
-
-    [ "", "./dbaseadm.py -k test3 -a testdata3", b""],
-    [ "", "./dbaseadm.py -k test4 -a testdata4", b""],
-    [ "", "./dbaseadm.py -k test5 -a testdata5", b""],
-
-    ["Get data", "./dbaseadm.py -t test",
-         b"[[b'test', b'testdata']]\n", ],
-
-    ["Find data", "./dbaseadm.py -F test",
-        b"[[b'test5', b'testdata5'], [b'test4', b'testdata4'], "\
-        b"[b'test3', b'testdata3'], [b'test2', b'testdata2'], "\
-        b"[b'test', b'testdata']]\n", ],
-    ]
+Version = "1.0.0"
 
 def strdiff(expectx, actualx):
+
+    ''' Rudimentary info on string differences '''
+
     strx = ""
     for cnt, bb in enumerate(expectx):
         #print("cnt", cnt, bb)
@@ -55,40 +32,57 @@ def strdiff(expectx, actualx):
             break
     return strx
 
-def xdiff(expectx, actualx):
+def xdiff(actualx, expectx, findflag):
 
-    ''' Compare values, display string in Color '''
+    ''' Compare values, display string in Color
+        Sensitive to find flag.
+    '''
 
-    if expectx == actualx:
-        return "\033[32;1mOK\033[0m"
+    if findflag:
+        #print("Find", str(expectx), str(actualx))
+        if str(expectx) in str(actualx):
+            return "\033[32;1mOK\033[0m"
+        else:
+            return"\033[31;1mERR\033[0m"
+
     else:
-        return"\033[31;1mERR\033[0m"
+        if expectx == actualx:
+            return "\033[32;1mOK\033[0m"
+        else:
+            return"\033[31;1mERR\033[0m"
 
-def obtain(cmd):
+def obtain_response(cmd):
+
+    ''' Get output from command, if any '''
+
+    comm = [0,]
     exec = cmd.split()
     if args.debug > 1:
         print("exec:", exec)
-    ret = subprocess.Popen(exec, stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE)
-    comm = ret.communicate()
+
+    if exec:
+        try:
+            ret = subprocess.Popen(exec, stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE)
+            comm = ret.communicate()
+            if args.outp:
+                print(codecs.decode(comm[0]).replace("\\n", "\n"))
+                sys.stdout.flush()
+        except:
+            print("Cannot communicate with:", exec, file=sys.stderr)
+            print(sys.exc_info())
+
     return comm[0]
 
-# Fresh start, no data
-try:
-    os.remove("pydbase.pydb")
-    os.remove("pydbase.pidx")
-except:
-    pass
+def send_expect(context, sendx, expectx, findflag):
 
-def send_expect(context, sendx, expectx):
+    ''' Evaluate one SEND -- EXPECT sequence '''
 
-    ''' evaluate Send -- EXPECT sequence '''
-
-    ret = obtain(sendx)
+    ret = obtain_response(sendx)
     if args.debug > 2:
         print("\033[32;1mGot: ", ret, "\033[0m")
 
-    err = xdiff(ret, expectx)
+    err = xdiff(ret, expectx, findflag)
 
     # If no context, we do not want any printing
     if context:
@@ -107,49 +101,73 @@ def send_expect(context, sendx, expectx):
         if ret != expectx:
             print("\033[34;1mDiff:\033[0m\n",
                 strdiff(ret, expectx))
+    return err
 
 def mainloop():
 
-    global test_case
+    global args
 
-    if args.readx:
-        try:
-            with open(args.readx) as fp:
-                testx = fp.read()
-        except:
-            print("Cannot open file", "'" + args.readx + "'")
-            sys.exit()
-        test_case = eval(testx)
-
-    for aa in test_case:
-        send_expect(aa[0], aa[1], aa[2])
-
-version = "1.0.0"
+    if args.test_cases:
+        for fff in args.test_cases:
+            try:
+                with open(fff) as fp:
+                    testx = fp.read()
+            except:
+                print("Cannot open file", "'" + fff  + "'", file=sys.stderr)
+                args.errcnt += 100
+                #sys.exit()
+                continue
+            #print("testx", testx)
+            try:
+                test_case_code = eval(testx)
+            except:
+                print("Error in", fff, sys.exc_info(), file=sys.stderr)
+                args.errcnt += 1
+                continue
+            for aa in test_case_code:
+                err = send_expect(aa[0], aa[1], aa[2], aa[3])
+                #print("err", err)
+                if "ERR" in err:
+                    args.errcnt += 1
 
 def mainfunct():
 
     global args
 
     parser = argparse.ArgumentParser(\
-            description='Test send/expect by executing sub commands')
+            description='Test send/expect by executing sub commands.')
 
+    parser.add_argument("-V", '--version', dest='version',
+                        default=0,  action='store_true',
+                        help='Show version number')
     parser.add_argument("-v", '--verbose', dest='verbose',
                         default=0,  action='count',
-                        help='verbocity on (default: off)')
-
-    parser.add_argument("-r", '--read', dest='readx',
-                        default=0,  action='store',
-                        help='Read test from file)')
-
+                        help='increase verbocity (Default: none)')
     parser.add_argument("-d", '--debug', dest='debug',
                         default=0,  type=int, action='store',
-                        help='Debug level')
+                        help='Debug level. Default: off')
+    parser.add_argument("-o", '--output', dest='outp',
+                        default=0,  action='store_true',
+                        help='Show communcation to program. Default: off')
+    parser.add_argument("test_cases", nargs= "*",
+                        help = "Test case file names to execute")
 
     args = parser.parse_args()
     #print(args)
+
+    if args.version:
+        print("Version: %s" % Version)
+        sys.exit(0)
+
+    if not args.test_cases:
+        print("Must specify at least one test case file.")
+        sys.exit(1)
+
+    args.errcnt = 0
     mainloop()
 
 if __name__ == "__main__":
     mainfunct()
+    sys.exit(args.errcnt)
 
 # EOF
